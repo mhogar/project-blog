3809066baba3c7d1353fff25e4f7018fd981e097
Rails Udemy Course <railsudemycourse@gmail.com>
2018-04-28 04:28:05 +0000

    Updated user profile show styling and UI

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,9 +1,13 @@
 class UserProfile < ActiveRecord::Base
   belongs_to :user
   
-  validates :about, allow_blank: true, length: { minimum: 10, maximum: 300 }
+  validates :about, allow_blank: true, length: { minimum: 10, maximum: 500 }
   validates :first_name, allow_blank: true, length: { maximum: 15 }
   validates :last_name, allow_blank: true, length: { maximum: 15 }
   validates :occupation, allow_blank: true, length: { maximum: 20 }
   validates :user_id, presence: true
+  
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
 end
