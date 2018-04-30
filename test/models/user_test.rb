08c13c99fc12266333c36b8605237d18a96efdc1
Rails Udemy Course <railsudemycourse@gmail.com>
2018-04-30 17:50:09 +0000

    Added user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(email: "user@example.com", password: "password", user_name: "user")
+  end
+  
+  test "user is valid" do
+    assert @user.valid?
+  end
+  
+  test "user name should be present" do
+    @user.user_name = nil
+    assert_not @user.valid?
+  end
+  
+  test "user name should be unique" do
+    user2 = User.new(email: "user2@example.com", password: "password", user_name: "user")
+    user2.save
+    
+    assert_not @user.valid?
+  end
+  
+  test "user name should not be too short" do
+    @user.user_name = "a"
+    assert_not @user.valid?
+  end
+  
+  test "user name should not be too long" do
+    @user.user_name = "a" * (get_validation_max(User, :user_name) + 1)
+    assert_not @user.valid?
+  end
+  
+  test "user name should be of the correct format" do
+    @user.user_name = "user*;%@${]?<>.,"
+    assert_not @user.valid?
+  end
 end
