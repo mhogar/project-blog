7c90193564bea4fb03a4362962cf9642ca7bf43c
Rails Udemy Course <railsudemycourse@gmail.com>
2018-05-02 19:20:27 +0000

    Updated migration files

--- db/migrate/20180426151238_remove_icon_from_language.rb
+++ db/migrate/20180426151238_remove_icon_from_language.rb
@@ -1,7 +1,5 @@
 class RemoveIconFromLanguage < ActiveRecord::Migration
   def change
     remove_column :languages, :icon, :string
-    add_reference :languages, :languages_icon, index: true
-    add_foreign_key :languages, :languages_icons
   end
 end
--- db/migrate/20180426152605_remove_language_icon_id_from_language.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLanguageIconIdFromLanguage < ActiveRecord::Migration
-  def change
-    remove_column :languages, :languages_icon_id, :integer
-  end
-end
